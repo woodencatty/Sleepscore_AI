b2e29a34083a5face6a2a7e4954b0c366a1f9005
WoodenCatty <WoodenCatty@gmail.com>
2019-11-20 21:16:13 +0900

    어헣ㅎ헣허헣ㅎ헣허

--- test/dbconn.js
+++ test/dbconn.js
@@ -1,4 +1,5 @@
-const USER_INFO_TABLE = "user_info"
+var User_Number = 139;
+const fs = require('fs');
 
 const dbPoolByMySql2Module = require('mysql2/promise').createPool({
     host : "sleepcaredb.cehn5ondbm2b.ap-northeast-2.rds.amazonaws.com",
@@ -9,20 +10,27 @@ const dbPoolByMySql2Module = require('mysql2/promise').createPool({
 
 const selectSangHunByMySql2Module = async () => {
     console.log("MySql2 Module을 이용한 Query")
+
     const dbConnectionByMySql2Module = await dbPoolByMySql2Module.getConnection(async conn => conn)
     try {
-        let [userInfoOfSangHun] = await dbConnectionByMySql2Module.query(
-                `SELECT * FROM ${USER_INFO_TABLE}
-                WHERE userName LIKE ?`,
-                ['이상훈%']
-            )
-        console.log(userInfoOfSangHun)
-        
+        let [traindata_sleepscore] = await dbConnectionByMySql2Module.query(
+                'SELECT temperature,humidity,light,sound,vibartion,posture,movementCount,snoringCount FROM sleep_info WHERE userInfo_id LIKE ?',[User_Number]) // 10개만
+        console.log(traindata_sleepscore);
+
+        //FileWrite
+        const data = JSON.stringify(traindata_sleepscore);
+        fs.writeFile('data.txt', data+'\n', (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+          });
+
     } catch(selectionError) {
         console.log(selectionError)
     }
+    finally {
+        dbPoolByMySql2Module.end();
+        console.log("done");
+    }
 }
 
-selectSangHunByMySql2Module(()=>{
-    //connection.release()
-}) // <- 2. mysql2
+selectSangHunByMySql2Module();
